e29896fb054203c9de9055ff0728f60069113ffb
bkeener <bkeener7@gmail.com>
2023-01-23 11:21:30 -0700

    Add method to convert I numerals to an integer

--- lib/roman_conversion.rb
+++ lib/roman_conversion.rb
@@ -1,3 +1,17 @@
-class RomanConversion
-  def roman_to_int(s); end
+class RomanConversione
+  def roman_to_int(s)
+    s.chars.map { |numeral| dictionary[numeral] }.sum
+  end
+
+  def dictionary
+    {
+      'I' => 1,
+      'V' => 5,
+      'X' => 10,
+      'L' => 50,
+      'C' => 100,
+      'D' => 500,
+      'M' => 1000
+    }
+  end
 end
--- spec/roman_conversion_spec.rb
+++ spec/roman_conversion_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe RomanConversion do
   let(:conversion) { RomanConversion.new }
+  let(:case1) { 'III' }
 
   it 'exists' do
     expect(conversion).to be_an_instance_of(RomanConversion)
+  end
+
+  it 'can convert I numerals to numbers' do
+    expect(conversion.roman_to_int('III')).to eq(3)
   end
 end
